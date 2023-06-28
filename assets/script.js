ea7ae8f31ee7386aee6ac35aaf4ecbae8a530cc9
Leila <farahani.wrk@gmail.com>
2023-06-28 14:43:57 +0330

    Added swiper and fixed login page's problems

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,7 @@ const USERS = [
 const paragragh = document.createElement('p');
 
 const greeting = document.getElementById('greeting');
-const logout = document.getElementById('loguot');
+const logout = document.getElementById('logout');
 
 const user = document.getElementById('InputName1');
 const pass = document.getElementById('InputPassword1');
@@ -27,35 +27,47 @@ const btn = document.getElementById('btn');
 
 
 btn.addEventListener('click', (e) => {
+
     e.preventDefault();
     paragragh.innerText = '';
     document.body.appendChild(paragragh);
+
     let i = 0;
 
-    USERS.map(el => {
+    if (JSON.parse(window.localStorage.getItem('userName')) !== null) {
+        const userName = JSON.parse(window.localStorage.getItem('userName'));
 
-        if (user.value === el.inputName && pass.value === el.inputPass) {
+        paragragh.innerText = `${userName} جان، خوش آمدید`;
+        document.body.appendChild(paragragh);
+    }
+    else {
+        USERS.map(el => {
 
-            window.localStorage.setItem('userName', JSON.stringify(user.value));
-            user.value = '';
-            pass.value = '';
+            if (user.value === el.inputName && pass.value === el.inputPass) {
 
-            if (JSON.parse(window.localStorage.getItem('userName')) !== null) {
-                const userName = JSON.parse(window.localStorage.getItem('userName'));
+                window.localStorage.setItem('userName', JSON.stringify(user.value));
+                user.value = '';
+                pass.value = '';
 
-                paragragh.innerText = `${userName} جان، خوش آمدید`;
-                document.body.appendChild(paragragh);
+                if (JSON.parse(window.localStorage.getItem('userName')) !== null) {
+                    const userName = JSON.parse(window.localStorage.getItem('userName'));
 
-            } else {
-                paragragh.innerText = ` شما هنوز وارد نشده اید`;
-                document.body.appendChild(paragragh);
+                    paragragh.innerText = `${userName} جان، خوش آمدید`;
+                    document.body.appendChild(paragragh);
+
+                } else {
+                    paragragh.innerText = ` شما هنوز وارد نشده اید`;
+                    document.body.appendChild(paragragh);
+                }
+
             }
+            else {
+                i++;
+            }
+        })
 
-        }
-        else {
-            i++;
-        }
-    })
+    }
+
 
     if (i === 3) {
         paragragh.innerText = `ابتدا اطلاعات خود را به شکل صحیح وارد نمایید`;
@@ -68,11 +80,53 @@ btn.addEventListener('click', (e) => {
 
 logout.addEventListener('click', () => {
 
-    window.localStorage.removeItem('userName');
-    paragragh.innerText = '';
-    paragragh.innerText = ` شما با موفقیت خارج شدید`;
-    document.body.appendChild(paragragh);
-    user.value = '';
-    pass.value = '';
-    logout.style.
+    if (JSON.parse(window.localStorage.getItem('userName')) !== null) {
+        window.localStorage.removeItem('userName');
+        paragragh.innerText = '';
+        paragragh.innerText = `شما با موفقیت خارج شدید`;
+        document.body.appendChild(paragragh);
+        user.value = '';
+        pass.value = '';
+    } else {
+        paragragh.innerText = `شما هنوز وارد نشدید`;
+        document.body.appendChild(paragragh);
+    }
+
 });
+
+
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     // direction: 'vertical',
+
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+
+//         type: "progressbar",
+
+//     },
+//     loop: true,
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+
+//     slidesPerView: 2,
+//     spaceBetween: 20,
+//     // effect: "cube",
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: true,
+//     },
+//     speed: 600,
+//     parallax: true,
+// });
+
